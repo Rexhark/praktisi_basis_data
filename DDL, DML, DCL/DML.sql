a245a21e1671aaccf6562988b8e47414558c9cdb
Mukramm_ <mukrammuhammad@gmail.com>
2022-10-08 23:44:31 +0800

    Ini DML-nya

---
+++ DDL, DML, DCL/DML.sql
@@ -0,0 +1,18 @@
+USE tugas_basis_data;
+INSERT INTO students VALUES 
+(1, "Muhammad Arif", "Belibis Street", "Sistem Informasi"),
+(2, "Dummy Siswa1", "Dummy Street1", "Sistem Informasi"),
+(3, "Dummy Siswa2", "Dummy Street2", "Sistem Informasi"),
+(4, "Dummy Siswa3", "Dummy Street3", "Sistem Informasi");
+
+INSERT INTO course VALUES
+(1, "Aljabar Linear", 90),
+(2, "Programming Web", 90),
+(3, "Basis Data", 90),
+(4, "Anilisis Perancangan", 90);
+
+INSERT INTO jadwal VALUES
+(1, "Aljabar Linear", '2022-1-12', '07:30', 1, 1),
+(2, "Programming Web", '2022-1-12', '08:30', 1, 2),
+(3, "Basis Data", '2022-1-12', '11:00', 1, 3),
+(4, "Analisis Perancangan", '2022-1-12', '13:30', 1, 4);
